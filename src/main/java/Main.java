f57f6e14db7341671274a1d2710d091fd6d0a89e
rashestripathy <your-email@example.com>
2026-06-19 11:54:08 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,5 +4,9 @@ public class Main {
     public static void main(String[] args) throws Exception {
         // TODO: Uncomment the code below to pass the first stage
         System.out.print("$ ");
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+        System.out.println(a);
+
     }
 }
