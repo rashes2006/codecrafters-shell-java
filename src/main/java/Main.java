506cfc1869464bc812c83a00aec010d1a5d7d05b
rashestripathy <your-email@example.com>
2026-06-19 12:09:03 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,6 +13,14 @@ public class Main {
             else if (a.startsWith("echo")){
                 System.out.println(a.substring(5));
             }
+            else if (a.startsWith("type")){
+                if (a.substring(5).equals("echo")){
+                    System.out.println("echo is a shell builtin");
+                }
+                else if (a.substring(5).equals("echo")){
+                    System.out.println("exit is a shell builtin");
+                }
+            }
             else {
             System.out.println(a + ": command not found");}
 
