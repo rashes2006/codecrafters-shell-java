ed9532cec755b6f803c510c9e22539bb168a8d33
rashestripathy <your-email@example.com>
2026-06-20 14:10:08 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,26 +1,40 @@
 import java.util.Scanner;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        // TODO: Uncomment the code below to pass the first stage
+        List<String> builtins = List.of("exit", "echo", "type");
+        Scanner sc = new Scanner(System.in);
+
         while (true) {
             System.out.print("$ ");
-            Scanner sc = new Scanner(System.in);
-            String a = sc.nextLine();
-            if (a.equals("exit")){
+            if (!sc.hasNextLine()) {
                 break;
             }
-            else if (a.startsWith("echo")){
-                System.out.println(a.substring(5));
+            String input = sc.nextLine().trim();
+            if (input.isEmpty()) {
+                continue;
             }
-            else if (a.startsWith("type")){
-                    System.out.println(a.substring(5)+ " is a shell builtin");
-                
-            }
-            else {
-            System.out.println(a + ": command not found");}
 
+            // Split the input into command and argument
+            String[] parts = input.split(" ", 2);
+            String command = parts[0];
+            String arg = parts.length > 1 ? parts[1] : "";
+
+            if (command.equals("exit")) {
+                break;
+            } else if (command.equals("echo")) {
+                System.out.println(arg);
+            } else if (command.equals("type")) {
+                if (builtins.contains(arg)) {
+                    System.out.println(arg + " is a shell builtin");
+                } else {
+                    System.out.println(arg + ": not found");
+                }
+            } else {
+                System.out.println(input + ": command not found");
+            }
         }
-
+        sc.close();
     }
 }
